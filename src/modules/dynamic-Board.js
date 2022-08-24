00d74eb3c6ded464bb3f109ee4617a836fe1929b
michael-duke <michael.21duke@gmail.com>
2022-08-24 11:19:26 +0300

    Remove id property when creating a score. Update render method
    - Omit id property since it won't be used by API
    - Include the id generated after API call to render the order of scores

--- src/modules/dynamic-Board.js
+++ src/modules/dynamic-Board.js
@@ -8,11 +8,11 @@ export default class DynamicBoard {
     const scoreDisplay = this.resetScoreDisplay();
 
     const message = document.createElement('span');
-    message.innerText = 'No winner yet in the Leaderboard. ';
+    message.innerText = 'No winners yet in the Leaderboard. ';
     message.className = 'empty-message italic';
 
     const smileyFace = document.createElement('span');
-    smileyFace.innerText = '😄 Go to Add score';
+    smileyFace.innerText = '😄 Go to Add score, or hit Refresh';
 
     scoreDisplay.append(message, smileyFace);
   }
@@ -26,11 +26,11 @@ export default class DynamicBoard {
   render(scores) {
     const scoreBoard = this.resetScoreDisplay();
     scores.forEach((score) => {
-      const { name, points } = score;
+      const { id, name, points } = score;
 
       const scoreMsg = document.createElement('h3');
       scoreMsg.classList = 'pl-3 even:bg-gray-300';
-      scoreMsg.innerText = `${name} :`;
+      scoreMsg.innerHTML = `<strong>${id}. </strong> ${name} :`;
 
       const pointsScored = document.createElement('span');
       pointsScored.classList = 'ml-2';
